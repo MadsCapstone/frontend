f6724f54fcb85d9a1908503c68f83d39d68f4ac0
Ermias Bizuwork <ebiz@ermiass-mbp.lan>
2021-08-19 20:18:15 -0400

    added worker class

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,7 +8,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
-
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 
 class VizMap extends Component {
